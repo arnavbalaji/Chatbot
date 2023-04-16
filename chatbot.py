e1cc8e2c63607ba0d9437c76ab1a40c26e9e6993
arnavbalaji <90121393+arnavbalaji@users.noreply.github.com>
2023-04-15 22:51:23 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -64,93 +64,74 @@ train_iterator, valid_iterator = Iterator.splits(
 import torch.nn as nn
 
 class Encoder(nn.Module):
-    def __init__(self, input_size, hidden_size, dropout=0.2):
-        super(Encoder, self).__init__()
-        
-        self.input_size = input_size
-        self.hidden_size = hidden_size
+    def __init__(self, input_dim, emb_dim, num_layers, dropout=0.2):
+        super().__init__()
+
+        self.input_dim = input_dim
+        self.emb_dim = emb_dim
+        self.num_layers = num_layers
         self.dropout = dropout
-        
-        self.embedding = nn.Embedding(self.input_size, self.hidden_size)
-        self.lstm = nn.LSTM(self.hidden_size, self.hidden_size, 2, dropout=self.dropout)
-        
+
+        self.embedding = nn.Embedding(input_dim, emb_dim)
+        self.lstm = nn.LSTM(emb_dim, emb_dim, num_layers, dropout=dropout, bidirectional=True)
+        self.dropout = nn.Dropout(dropout)
+    
+    def init_hidden(self, batch_size):
+        hidden = torch.zeros(self.num_layers, batch_size, self.emb_dim).to(device)
+        cell = torch.zeros(self.num_layers, batch_size, self.emb_dim).to(device)
+        return hidden, cell
+
     def forward(self, x, hidden, cell):
-        x = self.embedding(x)
-        x = x.unsqueeze(0)
-        
-        x, (hidden, cell) = self.lstm(x, (hidden, cell))
-        x = x.squeeze(0)
-        
+        x = self.dropout(self.embedding(x))
+        x, (hidden, cell) = self.lstm(x, hidden, cell)
         return x, hidden, cell
     
 class Decoder(nn.Module):
-    def __init__(self, hidden_size, output_size, dropout=0.2):
-        super(Decoder, self).__init__()
-        
-        self.hidden_size = hidden_size
-        self.output_size = output_size
+    def __init__(self, output_dim, hidden_dim, num_layers, dropout=0.2):
+        super().__init__()
+
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
         self.dropout = dropout
-        
-        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
-        self.lstm = nn.LSTM(self.hidden_size, self.hidden_size, 2, dropout=self.dropout)
-        self.fc = nn.Linear(self.hidden_size, self.output_size)
-        
-        self.softmax = nn.LogSoftmax(dim=1)
+
+        self.embedding = nn.Embedding(output_dim, hidden_dim)
+        self.lstm = nn.LSTM(hidden_dim, hidden_dim, num_layers, dropout=dropout)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
         
     def forward(self, x, hidden, cell):
         x = x.unsqueeze(0)
-        x = self.embedding(x)
-        x = x.view(1, x.size(0), -1)
+        x = self.dropout(self.embedding(x))
         x, (hidden, cell) = self.lstm(x, (hidden, cell))
-        x = self.fc(x)
-        x = self.softmax(x)
-        
+        x = self.fc(x.squeeze(0))
         return x, hidden, cell
 
 class Seq2Seq(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, dropout=0.2):
-        super(Seq2Seq, self).__init__()
+    def __init__(self, encoder, decoder):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, src, trg, teacher_forcing_ratio=0.5):
+        batch_size = src.shape[1]
+        trg_len = trg.shape[0]
+        trg_vocab_size = self.decoder.output_dim
+
+        outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(device)
+
+        hidden, cell = self.encoder.init_hidden(batch_size)
+
+        for i in range(src.shape[0]):
+            _, hidden, cell = self.encoder(src[i], hidden, cell)
         
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.output_size = output_size
-        self.dropout = dropout
-        
-        self.encoder = Encoder(self.input_size, self.hidden_size, self.dropout)
-        self.decoder = Decoder(self.hidden_size, self.output_size, self.dropout)
-    
-    def forward(self, src, trg, batch_size, teacher_force=0.5):
-        src_len = src.shape[0]
-        batch_size = trg.shape[1]
-        trg_vocab_size = self.output_size
+        x = torch.Tensor([[0]]).long().to(device)
 
-        encoder_hidden = torch.zeros([2, batch_size, self.hidden_size]).to(device) 
-        cell_state = torch.zeros([2, batch_size, self.hidden_size]).to(device)
-
-        encoder_outputs = torch.zeros(src_len, batch_size, self.hidden_size * 2).to(device)
-
-        for i in range(src_len):
-            encoder_output, encoder_hidden, cell_state = self.encoder(src[i], encoder_hidden, cell_state)
-            encoder_outputs[i] = encoder_output
-
-        decoder_hidden = encoder_hidden[:self.decoder.n_layers]
-        decoder_cell = torch.zeros_like(decoder_hidden).to(device)
-
-        outputs = torch.zeros(self.max_len, batch_size, trg_vocab_size).to(device)
-
-        if teacher_force:
-            trg_len = trg.shape[0]
-            for i in range(1, trg_len):
-                decoder_input = trg[i-1]
-                output, decoder_hidden, decoder_cell = self.decoder(decoder_input, decoder_hidden, decoder_cell, encoder_outputs)
-                outputs[i] = output
-        else:
-            decoder_input = trg[0]
-            for i in range(1, self.max_len):
-                output, decoder_hidden, decoder_cell = self.decoder(decoder_input, decoder_hidden, decoder_cell, encoder_outputs)
-                outputs[i] = output
-                top1 = output.argmax(1)
-                decoder_input = top1.unsqueeze(1)
+        for t in range(1, trg_len):
+            output, hidden, cell = self.decoder(x, hidden, cell)
+            outputs[t] = output
+            best_guess = output.argmax(1)
+            x = trg[t] if random.random() < teacher_forcing_ratio else best_guess
 
         return outputs
 
@@ -160,27 +141,27 @@ output_dim = len(answer_field.vocab)
 hidden_dim = 512
 dropout = 0.2
 
-model = Seq2Seq(input_dim, hidden_dim, output_dim, dropout).to(device)
+encoder = Encoder(input_dim, hidden_dim, 2, dropout)
+decoder = Decoder(output_dim, hidden_dim, 2, dropout)
+model = Seq2Seq(encoder, decoder).to(device)
+
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 PAD_IDX = answer_field.vocab.stoi['<pad>']
 criterion = nn.CrossEntropyLoss(ignore_index=PAD_IDX)
 
 num_epochs = 1
-
 for epoch in range(num_epochs):
-    model.train()
-    epoch_loss = 0
     for batch in train_iterator:
+        src = batch.src
+        trg = batch.trg
         optimizer.zero_grad()
-        outputs = model(batch.src, batch.trg, batch_size)
-        
-        outputs_flatten = outputs[1:].view(-1, outputs.shape[-1])
-        trg_flatten = batch.trg[1:].view(-1)
-        
-        loss = criterion(outputs_flatten, trg_flatten)
+        output = model(src, trg)
+        output_dim = output.shape[-1]
+        output = output[1:].view(-1, output_dim)
+        trg = trg[1:].view(-1)
+        loss = criterion(output, trg)
         loss.backward()
         optimizer.step()
-        
-        epoch_loss += loss.item()
-    print(f"Epoch: {epoch + 1}; Training Loss: {epoch_loss/len(train_iterator)}")
+
+    print(f'Epoch: {epoch+1:02} | Epoch Loss: {loss:.3f}')
